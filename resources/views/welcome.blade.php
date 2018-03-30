c9879a4e31d4d4cc32fc3c0f29f1817f3194365f
Emanuel Sosa <emanuelesausosa@gmail.com>
2018-03-30 07:19:07 -0600

    change main title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Question blog
                 </div>
 
                 <div class="links">
